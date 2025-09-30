7a5fe62aa44fdb7af5a213dd23dc3620a728a119
jairsrib <182438105+LucasBoesel@users.noreply.github.com>
2025-09-29 21:10:35 -0300

    aula04

--- js/firebaseConfig.js
+++ js/firebaseConfig.js
@@ -5,7 +5,14 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/11.6.0/firebase
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-
+ apiKey: "AIzaSyBwliUDUiq27_KOGt84UczQct6Z8X3rsyQ",
+  authDomain: "projeto-teste-8abb5.firebaseapp.com",
+  databaseURL: "https://projeto-teste-8abb5-default-rtdb.firebaseio.com",
+  projectId: "projeto-teste-8abb5",
+  storageBucket: "projeto-teste-8abb5.firebasestorage.app",
+  messagingSenderId: "650943976996",
+  appId: "1:650943976996:web:fea4b31859f7ca0cf36ab1",
+  measurementId: "G-JLR92CM235"
 };
 
 // Initialize Firebase
